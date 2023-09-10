37885e98a10e1a208c02208059a54bcc252ea1c8
Acautorak <acislav@gmail.com>
2023-09-10 23:59:38 +0200

    s

---
+++ Assets/0_Scripts/Main/BuildingVisual.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingVisual : MonoBehaviour
+{
+    public Building building;
+    [SerializeField] private ProgressBar progressBar;
+    [SerializeField] private Image artworkImage;
+    [SerializeField] private Button upgradeButton;
+    [SerializeField] private Button specialButton;
+
+    private void Start()
+    {
+    }
+
+
+}
