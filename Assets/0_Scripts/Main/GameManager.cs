df277ca3e0194e40c80d25a7924a04ed186d4f41
Utorak <acislav@gmail.com>
2023-10-03 08:55:47 +0200

    ds

--- Assets/0_Scripts/Main/GameManager.cs
+++ Assets/0_Scripts/Main/GameManager.cs
@@ -45,7 +45,7 @@ public class GameManager : MonoSingletone<GameManager>
     public void AddGold(int value)
     {
         gold += value;
-        // dodao bb ss sas cocaine SS
+        // dodao bb ss sas cocaine SS ds
     }
 
 }
