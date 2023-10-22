67924cc0044d97b4d9ff58862c5f66754386f901
Utorak <acislav@gmail.com>
2023-10-22 20:20:27 +0200

    Ui import and UIManager

---
+++ Assets/0_Scripts/Main/UiManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiManager : MonoBehaviour
+{
+    [SerializeField] private RectTransform blackFadePanel;
+
+    [SerializeField] private Button buildBtn, eventBtn, settingsBtn;
+    
+}
