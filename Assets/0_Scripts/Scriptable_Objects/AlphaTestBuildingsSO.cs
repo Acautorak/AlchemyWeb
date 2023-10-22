35b945a472c2b8dd860abe4355fbb01e3b97fec4
Utorak <acislav@gmail.com>
2023-10-22 18:51:44 +0200

    form

--- Assets/0_Scripts/Scriptable_Objects/AlphaTestBuildingsSO.cs
+++ Assets/0_Scripts/Scriptable_Objects/AlphaTestBuildingsSO.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class AlphaTestBuildingsSO : ScriptableObject
 {
     [SerializeField] List<Building> buildings;
-    // ok art
+    // ok art from
 
     public List<Building> GetBuildings()
     {
