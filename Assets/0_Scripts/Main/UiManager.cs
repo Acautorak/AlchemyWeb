8a96a7318262c16adc6681249f28d330714eab3c
Utorak <acislav@gmail.com>
2023-11-10 12:17:49 +0100

    dsd

--- Assets/0_Scripts/Main/UiManager.cs
+++ Assets/0_Scripts/Main/UiManager.cs
@@ -16,7 +16,7 @@ public class UiManager : MonoBehaviour
     private const float BLACKFADETIME =0.3f;
     
 
-    // 680 is width of the panel winows
+    // 680 is width of the panel
     private  Vector2 BUILDINGSOUTOFBOUNDS =new Vector2(-Screen.width - 680, 0f);
     private Vector2 originalBuildingsPanelPosition;
 
